2b2b9dad7732f9623d705616a30694e506345c38
toheartz <abiyyu202@gmail.com>
2025-12-02 20:44:09 +0700

    tambah powermenu, soalnya males mencet tombol power

---
+++ wofi/powermenu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+choice=$(printf "Shutdown\nReboot\nLogout\nSuspend\nLock" | wofi --dmenu --prompt "Power")
+
+case "$choice" in
+Shutdown)
+  systemctl poweroff
+  ;;
+
+Reboot)
+  systemctl reboot
+  ;;
+
+Logout)
+  # Niri logout
+  if command -v niri session >/dev/null 2>&1; then
+    niri session close-all
+    exit
+  fi
+
+  # Hyprland logout
+  if command -v hyprctl >/dev/null 2>&1; then
+    hyprctl dispatch exit
+    exit
+  fi
+
+  # Fallback universal
+  loginctl terminate-user "$USER"
+  ;;
+
+Suspend)
+  systemctl suspend
+  ;;
+
+Lock)
+  # Eksekusi script lockscreen milikmu
+  ~/.config/hyprland/lockscreen
+  ;;
+esac
