4bd3b3959481dc7ca0442c812d9bcef2a9aa974c
toheartz <abiyyu202@gmail.com>
2025-12-17 21:26:35 +0700

    ganti rounding

--- hypr/toggle-gapless.sh
+++ hypr/toggle-gapless.sh
@@ -5,7 +5,7 @@ STATE_FILE=/tmp/hypr-gapstate
 if [ -f "$STATE_FILE" ]; then
   hyprctl keyword general:gaps_in 5
   hyprctl keyword general:gaps_out 9
-  hyprctl keyword decoration:rounding 0
+  hyprctl keyword decoration:rounding 4
   rm "$STATE_FILE"
   notify-send "Gapless disable"
 else
