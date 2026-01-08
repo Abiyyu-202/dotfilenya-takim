ba951de13780c013457f2eefdfca1730503ed2ee
toheartz <abiyyu202@gmail.com>
2026-01-08 12:21:36 +0700

    refactor, soalnya kemarin ga work abis update hyprland

--- wofi/change-wallpaper.sh
+++ wofi/change-wallpaper.sh
@@ -1,42 +1,33 @@
 #!/bin/bash
 
-WALLPAPER_DIR="$HOME/.config/hypr/wallpaper/"
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 
-SELECTED=$(ls "$WALLPAPER_DIR" | wofi --dmenu --prompt "Pilih wallpaper:")
-[ -z "$SELECTED" ] && exit
+if [ -z "$WAYLAND_DISPLAY" ]; then
+  WAYLAND_DISPLAY=$(ls "$XDG_RUNTIME_DIR" | grep -E '^wayland-[0-9]+' | head -n1)
+  export WAYLAND_DISPLAY
+fi
+
+SWWW="/usr/bin/swww"
+WOFI="/usr/bin/wofi"
+HYPRCTL="/usr/bin/hyprctl"
+JQ="/usr/bin/jq"
+
+WALLPAPER_DIR="$HOME/.config/hypr/wallpaper"
+
+SELECTED=$(
+  ls "$WALLPAPER_DIR" | $WOFI --dmenu --prompt "Pilih wallpaper:"
+)
+
+[ -z "$SELECTED" ] && exit 0
 
 WALLPAPER="$WALLPAPER_DIR/$SELECTED"
 
 cp "$WALLPAPER" "$HOME/.cache/wallpaper_rn.png"
 echo "$WALLPAPER" >"$HOME/.current_wallpaper"
 
-if ! pgrep -x "swww-daemon" >/dev/null; then
-  swww-daemon &
-  sleep 0.5
-fi
-
-SESSION=$(echo "$XDG_CURRENT_DESKTOP" | tr '[:upper:]' '[:lower:]')
-
-if [[ "$SESSION" == *"hyprland"* ]]; then
-  # HYPRLAND
-  MONITORS=$(hyprctl monitors -j | jq -r '.[].name')
-
-  for MON in $MONITORS; do
-    swww img "$WALLPAPER" \
-      --outputs "$MON" \
-      --transition-type grow \
-      --transition-step 120 \
-      --transition-fps 120 \
-      --transition-duration 1 \
-      --transition-bezier 0.4,0.2,0.2,1.0
-  done
-
-else
-  # NIRI
-  swww img "$WALLPAPER" \
-    --transition-type grow \
-    --transition-step 120 \
-    --transition-fps 120 \
-    --transition-duration 1 \
-    --transition-bezier 0.4,0.2,0.2,1.0
-fi
+$SWWW img "$WALLPAPER" \
+  --transition-type grow \
+  --transition-step 120 \
+  --transition-fps 120 \
+  --transition-duration 1 \
+  --transition-bezier 0.4,0.2,0.2,1.0
